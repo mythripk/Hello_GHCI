6c502699ab423c6caec7e12045b61873abcd8357
Mythri P K <mythri.p.k@intel.com>
2015-11-19 00:49:27 +0530

    Hello_GHCI: Print Hello string 10 times
    Use for loop to print hello string 10 times

--- hello_ghci.c
+++ hello_ghci.c
@@ -3,8 +3,11 @@
 int main()
 {
 	char string[] = "Hello GHCI";
+	int i, count = 10;
 
-	printf("%s\n", string);
+	/* Print string "Hello_GHCI" count times */
+	for(i = 0; i < count; i++)
+		printf("%s\n", string);
 
 	return 0;
 }
